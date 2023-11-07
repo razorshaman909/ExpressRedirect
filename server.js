a45db6bd02f35ea44a41507e411b6208ed430f4e
Andrew <work909ang@gmail.com>
2023-11-07 18:02:24 +0800

    first push

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const port = process.env.PORT || 3000;
+const url =
+  "YOUR_SHEET_LINK_WITH_API";
+// const getdata = () =>
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((jsonData) => {
+//       const [, keys, ...rows] = jsonData.values;
+//       const result = {};
+//       rows.forEach((row) => {
+//         const key = row[0];
+//         const value = row[1];
+//         result[key] = value;
+//       });
+//       return result;
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching JSON data:", error);
+//     });
+// const dataPromise = getdata();
+// const redirects = JSON.parse(fs.readFileSync('redirects.json', 'utf-8'));
+// const redirects = dataPromise.then((data) => console.log(data))
+
+// Redirect route handler
+
+app.get("/:redirectKey", (req, res) => {
+  const { redirectKey } = req.params;
+  const getdata = () =>
+    fetch(url)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        const [, keys, ...rows] = jsonData.values;
+        const result = {};
+        rows.forEach((row) => {
+          const key = row[0];
+          const value = row[1];
+          result[key] = value;
+        });
+        return result;
+      })
+      .catch((error) => {
+        console.error("Error fetching JSON data:", error);
+      });
+  const dataPromise = getdata();
+  dataPromise.then((redirects) => {
+    if (redirects[redirectKey]) {
+      res.redirect(redirects[redirectKey]);
+    } else {
+      // Handle 404 errors
+      res.status(404).send("Not Found");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
